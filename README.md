772cf73b3e6ba02142cc2972e4b698ada24c5914
Yves Kleny <yvesk.martins@gmail.com>
2025-03-25 14:32:29 -0300

    configurações iniciais do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # gameguides
+
+De inicio, a ideia é contruir um pequeno espaço na internet onde as pessoas possam compartilhar algum conhecimento sobre o jogo que elas amam.
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <h1>
+      De inicio, a ideia é contruir um pequeno espaço na internet onde as
+      pessoas possam compartilhar algum conhecimento sobre algum jogo que elas
+      amam.
+    </h1>
+  );
+}
+
+export default Home;
